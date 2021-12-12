7ae52cdd2e25f4d591b6c7515ced74176f42c460
ithrael <wh01096045@gmail.com>
2021-12-13 01:53:09 +0800

    test: message tab

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -1,33 +1,34 @@
 package burp;
 
-import burp.utils.OKHttpUtils;
-import com.alibaba.fastjson.JSONObject;
-
-import java.awt.*;
-import java.io.IOException;
+import java.awt.Component;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 
 public class BurpExtender implements IBurpExtender, IMessageEditorTabFactory {
     private IBurpExtenderCallbacks callbacks;
     private IExtensionHelpers helpers;
     private PrintWriter stdout;
-    BurpHelper burpHelper;
     private String host = "47.112.115.242:8089/sys/login";
     private String decryptUrl = "http://127.0.0.1:5000/decrypt";
     private final String BURP_PROXY_TAB_NAME = "burp decrypt proxy";
 
+    //
+    // implement IBurpExtender
+    //
+
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
-        callbacks.setExtensionName("Burp Proxy");
-        callbacks.printOutput("load success");
+        // keep a reference to our callbacks object
         this.callbacks = callbacks;
-        // obtain our output stream
+
+        // obtain an extension helpers object
+        helpers = callbacks.getHelpers();
         stdout = new PrintWriter(callbacks.getStdout(), true);
 
-        // register ourselves as an HTTP listener
-        burpHelper = new BurpHelper(callbacks.getHelpers());
-        helpers = burpHelper.helpers;
+        // set our extension name
+        callbacks.setExtensionName(BURP_PROXY_TAB_NAME);
+
+        // register ourselves as a message editor tab factory
+        callbacks.registerMessageEditorTabFactory(this);
     }
 
     //
@@ -35,8 +36,7 @@ public class BurpExtender implements IBurpExtender, IMessageEditorTabFactory {
     //
 
     @Override
-    public IMessageEditorTab createNewInstance(IMessageEditorController controller, boolean editable)
-    {
+    public IMessageEditorTab createNewInstance(IMessageEditorController controller, boolean editable) {
         // create a new instance of our custom editor tab
         return new BurpDecrptProxyTab(controller, editable);
     }
@@ -45,14 +45,12 @@ public class BurpExtender implements IBurpExtender, IMessageEditorTabFactory {
     // class implementing IMessageEditorTab
     //
 
-    class BurpDecrptProxyTab implements IMessageEditorTab
-    {
+    class BurpDecrptProxyTab implements IMessageEditorTab {
         private boolean editable;
         private ITextEditor txtInput;
         private byte[] currentMessage;
 
-        public BurpDecrptProxyTab(IMessageEditorController controller, boolean editable)
-        {
+        public BurpDecrptProxyTab(IMessageEditorController controller, boolean editable) {
             this.editable = editable;
 
             // create an instance of Burp's text editor, to display our deserialized data
@@ -65,51 +63,35 @@ public class BurpExtender implements IBurpExtender, IMessageEditorTabFactory {
         //
 
         @Override
-        public String getTabCaption()
-        {
+        public String getTabCaption() {
             return BURP_PROXY_TAB_NAME;
         }
 
         @Override
-        public Component getUiComponent()
-        {
+        public Component getUiComponent() {
             return txtInput.getComponent();
         }
 
         @Override
-        public boolean isEnabled(byte[] content, boolean isRequest)
-        {
+        public boolean isEnabled(byte[] content, boolean isRequest) {
             // enable this tab for requests containing a data parameter
-            stdout.println(helpers.analyzeRequest(content).getUrl().toString().contains(host));
-            return isRequest && helpers.analyzeRequest(content).getUrl().toString().contains(host);
+//            String requestUrl = helpers.analyzeRequest(content).getUrl().toString();
+            stdout.println(helpers.analyzeRequest(content).getHeaders());
+            return true;
         }
 
         @Override
-        public void setMessage(byte[] content, boolean isRequest)
-        {
-            if (content == null)
-            {
+        public void setMessage(byte[] content, boolean isRequest) {
+            if (content == null) {
                 // clear our display
                 txtInput.setText(null);
                 txtInput.setEditable(false);
-            }
-            else
-            {
+            } else {
                 // retrieve the data parameter
-                IParameter parameter = helpers.getRequestParameter(content, "username");
-                stdout.println(parameter.getValue());
-                String value = parameter.getValue();
-                String decryptBody = "test decryptBody";
-                try {
-                    decryptBody = OKHttpUtils.post(decryptUrl, "msg", value);
-                    stdout.println("decrypt: ");
-                    stdout.println("msg: " + decryptBody);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                IParameter parameter = helpers.getRequestParameter(content, "data");
 
                 // deserialize the parameter value
-                txtInput.setText(decryptBody.getBytes(StandardCharsets.UTF_8));
+                txtInput.setText(helpers.base64Decode(helpers.urlDecode(parameter.getValue())));
                 txtInput.setEditable(editable);
             }
 
@@ -118,30 +100,25 @@ public class BurpExtender implements IBurpExtender, IMessageEditorTabFactory {
         }
 
         @Override
-        public byte[] getMessage()
-        {
+        public byte[] getMessage() {
             // determine whether the user modified the deserialized data
-            if (txtInput.isTextModified())
-            {
+            if (txtInput.isTextModified()) {
                 // reserialize the data
                 byte[] text = txtInput.getText();
-                String input = text.toString();
+                String input = helpers.urlEncode(helpers.base64Encode(text));
 
                 // update the request with the new parameter value
-                return helpers.updateParameter(currentMessage, helpers.buildParameter("msg", input, IParameter.PARAM_BODY));
-            }
-            else return currentMessage;
+                return helpers.updateParameter(currentMessage, helpers.buildParameter("data", input, IParameter.PARAM_BODY));
+            } else return currentMessage;
         }
 
         @Override
-        public boolean isModified()
-        {
+        public boolean isModified() {
             return txtInput.isTextModified();
         }
 
         @Override
-        public byte[] getSelectedData()
-        {
+        public byte[] getSelectedData() {
             return txtInput.getSelectedText();
         }
     }
